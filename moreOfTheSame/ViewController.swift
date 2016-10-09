f08b34085b3b50e4b6aa6be56f98b8c3aa193815
Geo <giovannygaleano@gmail.com>
2016-10-09 15:08:17 -0400

    Initial Commit

---
+++ moreOfTheSame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moreOfTheSame
+//
+//  Created by Geovanny Galeano on 10/9/16.
+//  Copyright © 2016 Geovanny Galeano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
